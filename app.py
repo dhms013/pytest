08d3d66cb512192ffa8c75183cd53ef7f221398a
dhms013 <dhimas3593@gmail.com>
2025-11-08 05:36:01 +0700

    Update: Automatically open report on WSL

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
-from flask import Flask
+
 from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
-HOST = os.getenv('APP_HOST')
-PORT = int(os.getenv('APP_PORT'))
+HOST = os.getenv("APP_HOST")
+PORT = int(os.getenv("APP_PORT"))
 
-from routes.post_users import post_bp
-from routes.get_users import get_bp
-from routes.put_users import put_bp
 from routes.delete_users import delete_bp
+from routes.get_users import get_bp
+from routes.post_users import post_bp
+from routes.put_users import put_bp
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ app.register_blueprint(get_bp)
 app.register_blueprint(put_bp)
 app.register_blueprint(delete_bp)
 
-if __name__ == '__main__':
-    # Flask defaults to 127.0.0.1:5000. 
+if __name__ == "__main__":
+    # Flask defaults to 127.0.0.1:5000.
     # If you see 3000, you explicitly set it like this:
     app.run(debug=True, host=HOST, port=PORT)
